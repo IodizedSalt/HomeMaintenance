da98e590d269e5662909fa9522b86422553842b2
IodizedSalt <christophermoeller2000@gmail.com>
2023-07-26 14:47:57 +0200

    some css, js, favicons, and express

---
+++ src/app/app_local_dev.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require("path") 
+const port = 8000
+const routes = require("./routes")
+
+app.use(express.static(path.join(__dirname, '../.', '')));
+    
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
