e02d81fd472245c55e1839b9df0718c31256aa8e
IodizedSalt <christophermoeller2000@gmail.com>
2023-07-27 14:25:00 +0200

    make file adjustements

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
-upgrade
+upgrade:
 	docker build . -t christopher/homemaintenance
+	stopByPort.sh 8000
 	docker run -p 49160:8000 -d christopher/homemaintenance
---
+++ stopByPort.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for id in $(docker ps -q)
+do
+    if [[ $(docker port "${id}") == *"${1}"* ]]; then
+        echo "stopping container ${id}"
+        docker stop "${id}"
+    fi
+done
