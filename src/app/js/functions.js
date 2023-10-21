0f1b1bc298ef2d88877adbf2c47481cc01c797ed
IodizedSalt <christophermoeller2000@gmail.com>
2023-10-21 16:22:28 +0200

    CSS fix for responsive, non layout shifting dog; Also added input and random button for changing imgs

--- src/app/js/functions.js
+++ src/app/js/functions.js
@@ -50,6 +50,26 @@ function getISOWeekNumber(date) {
     return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
   }
 
+function setImgNum(num){
+    var file_name = document.getElementById('current_img').src.toString().split('/').slice(-1)[0];
+    var current_img_num = parseInt(file_name.split('.')[0])
+    if (!isNaN(num) && num != null && num != '') {
+        if(num <= parseInt(JSON.parse(sessionStorage.getItem("img_count")))){
+            document.querySelector("#current_img").src = '/app/img/piesku/' + num + '.jpg'
+        }else{
+            var alert_text = "There are only " + parseInt(JSON.parse(sessionStorage.getItem("img_count"))) + " images" 
+            alert(alert_text)
+            document.querySelector('#stefan_img_input').value =  current_img_num
+        }
+    }    
+}
+
+function randomImg(){
+    const img_count = JSON.parse(sessionStorage.getItem("img_count"));
+    var randomWholeNumber = Math.floor(Math.random() * img_count) + 1;
+    setImgNum(randomWholeNumber)
+}
+
 function forceCycle(image_type, increment_amnt) {
     var images = [];
     const img_count = JSON.parse(sessionStorage.getItem("img_count"));
@@ -93,6 +113,11 @@ function getImgCount() {
         if (response.ok) {
             response.json().then(img_count_res => {
                 sessionStorage.setItem("img_count", parseInt(img_count_res['file_count']));
+                // Set the max attribute based on the result of the getImgCount() function
+
+                document.querySelector('#stefan_img_input').setAttribute('placeholder',  "Choose a specific img number. There are only " + parseInt(img_count_res['file_count']) + " images!!");
+                document.querySelector('#stefan_img_input').setAttribute('max',  parseInt(img_count_res['file_count']));
+                document.querySelector('#stefan_img_input').setAttribute('min',  1);
             });
         }
     });
